ad42b5cfda860a05355a3c5ae0edf00ff3fe9c2b
Kulbhushan Sharma <kulbhushan.sharma@championx.com>
2026-06-08 23:56:53 +0530

    V03:Sidebar-layout

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,15 +1,27 @@
 // src/inngest/functions.ts
 import { inngest } from "./client";
+import {createGoogleGenerativeAI} from "@ai-sdk/google";
+import {generateText} from "ai";
 
-export const processTask = inngest.createFunction(
-  { id: "process-task", triggers: { event: "app/task.created" } },
+const google= createGoogleGenerativeAI();
+export const executeAI = inngest.createFunction(
+  { id: "execute-ai", triggers: { event: "execute/ai" } },
   async ({ event, step }) => {
-    const result = await step.run("handle-task", async () => {
-      return { processed: true, id: event.data.id };
-    });
-
-    await step.sleep("pause", "1s");
-
-    return { message: `Task ${event.data.id} complete`, result };
+    const {steps} = await step.ai.wrap("gemini-generated-text",
+       generateText,
+       {
+          model: google("gemini-2.5-flash"),
+          system: "You are a helpful assistant that summarizes text.",
+          prompt: "How  to find remote job in Reactjs?",
+          experimental_telemetry: {
+         isEnabled: true,
+         recordInputs: true,
+        
+         recordOutputs: true,
+  },
+       }
+    );
+    return steps;
+  
   }
 );
